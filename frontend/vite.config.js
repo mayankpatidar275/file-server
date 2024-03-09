8d979de2facb69f1fa6d9a3e27589ea8f39f10f6
Mayank Patidar <mayankpatidar275@gmail.com>
2024-03-09 21:22:17 +0530

    test: using dummy json in dev and DATA in dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist' // Production build output directory
+  },
+  server: {
+    fs: {
+      strict: false,
+      // Specify a different directory for development builds
+      // Note: This is optional and generally not recommended for development
+      base: 'dev'
+    }
+  }
+});
