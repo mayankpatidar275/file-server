7efa3fbbb468b2c17325ee5629a8252f5d4f7b8f
Mayank Patidar <mayankpatidar275@gmail.com>
2024-04-23 09:15:22 +0530

    fix: persisting the explorer on tab  switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Client from "./components/client/Client";
@@ -8,7 +8,6 @@ function App() {
   const [activeTab, setActiveTab] = useState("server");
   const [server, setServer] = useState(null);
 
-  // const [lastFetch, setLastFetch] = useState(null);
   return (
     <div className="flex flex-col h-screen">
       {/* Navbar with a certain height */}
@@ -18,11 +17,12 @@ function App() {
 
       {/* Content container takes the rest of the height */}
       <div className="flex-1">
-        {activeTab === "server" ? (
+        <div className={activeTab === "server" ? "block" : "hidden"}>
           <Server server={server} setServer={setServer} />
-        ) : (
+        </div>
+        <div className={activeTab === "client" ? "block" : "hidden"}>
           <Client />
-        )}
+        </div>
       </div>
     </div>
   );
