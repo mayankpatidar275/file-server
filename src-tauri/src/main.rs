cbb9b294f0af831ec230f4e60642626e00be5c1c
Mayank Patidar <mayankpatidar275@gmail.com>
2024-05-07 10:00:54 +0530

    test: main.rs changed

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,39 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod broadcast;
-mod discover;
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-
-#[tauri::command]
-async fn start_broadcasting() {
-    println!("inside start_broadcasting");
-    broadcast::start_broadcasting().await;
-}
-
-#[tauri::command]
-async fn stop_broadcasting() {
-    print!("iside stop_broadcasting");
-    broadcast::stop_broadcasting().await;
-}
-
-#[tauri::command]
-async fn start_discovering() {
-    println!("inside start_discovering");
-    discover::start_discovering().await;
-}
 
 fn main() {
     // Prevents additional console window on Windows in release, DO NOT REMOVE!!
     #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            start_broadcasting,
-            stop_broadcasting,
-            start_discovering
-        ])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
