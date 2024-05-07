4d7d71ccd04e827b66240a225d271d2c61b42557
Mayank Patidar <mayankpatidar275@gmail.com>
2024-05-07 09:47:47 +0530

    fix: vite config changed

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,16 @@ export default defineConfig(async () => ({
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
+    // to access the Tauri environment variables set by the CLI with information about the current target
+    envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
+    build: {
+      // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+      target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+      // don't minify for debug builds
+      minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+      // produce sourcemaps for debug builds
+      sourcemap: !!process.env.TAURI_DEBUG,
+    },
   },
 }));
 
