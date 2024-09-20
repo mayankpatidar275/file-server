9a446e908069272b428ee6bd4c128507c7efbe6f
Mayank Patidar <76393164+mayankpatidar275@users.noreply.github.com>
2024-09-20 09:47:22 +0530

    fix: no selected server conditionally

--- src/components/client/ClientExplorer.jsx
+++ src/components/client/ClientExplorer.jsx
@@ -10,19 +10,23 @@ const ClientExplorer = ({ ip, port }) => {
 
   useEffect(() => {
     // fetch(lastFetch ? lastFetch : `http://${ip}:${port}`)
-    fetch(`http://${ip}:${port}`)
-      .then((res) => {
-        const isHTML = res.headers.get("content-type").includes("text/html");
-        setIsFolder(isHTML);
-        return res.text();
-      })
-      .then((data) => {
-        setFileContent(isFolder ? "" : data);
-      })
-      .catch((error) => {
-        setIsFetchFailed(true);
-        console.error("Error fetching data:", error);
-      });
+    if (ip && port) {
+      fetch(`http://${ip}:${port}`)
+        .then((res) => {
+          const isHTML = res.headers.get("content-type").includes("text/html");
+          setIsFolder(isHTML);
+          return res.text();
+        })
+        .then((data) => {
+          setFileContent(isFolder ? "" : data);
+        })
+        .catch((error) => {
+          setIsFetchFailed(true);
+          console.error("Error fetching data:", error);
+        });
+    } else {
+      console.log("ip or port not found");
+    }
   }, [ip, port, isFolder]);
 
   // const getFileContent = (p) => {
@@ -38,35 +42,44 @@ const ClientExplorer = ({ ip, port }) => {
 
   return (
     <div className="flex flex-col   bg-gray-50">
-      {!isFetchFailed ? (
-        <div className="flex-1 flex">
-          <div className="w-1/3 bg-gray-200 h-[70vh] overflow-y-auto p-4">
-            <FolderAndFiles
-              // getFileContent={getFileContent}
-              isFolder={true}
-              name={"dufsRoot"}
-              p={filePath}
-              setFileContent={setFileContent}
-              searchIp={ip}
-              searchPort={port}
-              setFilePath={setFilePath}
-              // setLastFetch={setLastFetch}
-            />
-          </div>
-          <div className="w-2/3 bg-white ">
-            {filePath !== "/" ? (
-              <FileViewer fileUri={`http://${ip}:${port}${filePath}`} />
-            ) : (
-              <div className="flex justify-center items-center h-screen  ">
-                No file selected
-              </div>
-            )}
-          </div>
+      {!ip && !port && (
+        <div className="flex justify-center items-center bg-white text-gray-500 h-40">
+          No server selected
         </div>
+      )}
+      {ip && port ? (
+        !isFetchFailed ? (
+          <div className="flex-1 flex">
+            <div className="w-1/3 bg-gray-200 h-[80vh] overflow-y-auto p-4">
+              <FolderAndFiles
+                // getFileContent={getFileContent}
+                isFolder={true}
+                name={"dufsRoot"}
+                p={filePath}
+                setFileContent={setFileContent}
+                searchIp={ip}
+                searchPort={port}
+                setFilePath={setFilePath}
+                // setLastFetch={setLastFetch}
+              />
+            </div>
+            <div className="w-2/3 bg-white ">
+              {filePath !== "/" ? (
+                <FileViewer fileUri={`http://${ip}:${port}${filePath}`} />
+              ) : (
+                <div className="flex justify-center items-center h-[80vh]">
+                  No file selected
+                </div>
+              )}
+            </div>
+          </div>
+        ) : (
+          <div className="flex justify-center items-center h-screen  ">
+            No response
+          </div>
+        )
       ) : (
-        <div className="flex justify-center items-center h-screen  ">
-          No response
-        </div>
+        ""
       )}
     </div>
   );
